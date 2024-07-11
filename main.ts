ba5290f6f6a42ed7826b76520d3c1608cb1cfec5
Dev MakerLabVN <dev.makerlabvn@gmail.com>
2024-07-11 15:39:30 +0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+// Xóa toàn bộ nội dung trên LCD (nếu có)
+lcd.clearScreen()
+// Cho hiển thị nội dung lên màn hình LCD
+// Display text on the LCD
+lcd.displayText("Distance:", 1, 1)
 basic.forever(function () {
-	
+    // Hiển thị giá trị cảm biến lên màn hình LCD
+    // Display sensor value
+    lcd.displayText("" + ultraSonic.readDistance(ultraSonic.PingUnit.Centimeters, ultraSonic.PinKit.Port1) + " cm   ", 7, 1)
+    // Dừng 0.5s
+    // Pause 0.5s
+    basic.pause(500)
 })
